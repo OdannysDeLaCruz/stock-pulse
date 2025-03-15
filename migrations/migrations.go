dbad9147430e3c84f27c559f5cfa8003cf3076a4
Odannys Enrique De La Cruz Calvo <el_odanis321@hotmail.com>
2025-03-15 17:30:42 -0500

    feat (migrations): agregar columnas target_from y target_to a historial de precios

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -10,24 +10,23 @@ import (
 
 type Stock struct {
 	gorm.Model
-	Ticker      string `gorm:"uniqueIndex"`
-	TargetFrom  string `gorm:"column:target_from;not null"`
-	TargetTo    string `gorm:"column:target_to;not null"`
-	Company     string `gorm:"not null"`
-	Action      string `gorm:"not null"`
-	Brokerage   string `gorm:"not null"`
+	Ticker      string `gorm:"column:ticker;uniqueIndex"`
+	TargetFrom  float64 `gorm:"column:target_from;not null"`
+	TargetTo    float64 `gorm:"column:target_to;not null"`
+	Company     string `gorm:"column:company;not null"`
+	Action      string `gorm:"column:action;not null"`
+	Brokerage   string `gorm:"column:brokerage;not null"`
 	RatingFrom  string `gorm:"column:rating_from;not null"`
 	RatingTo    string `gorm:"column:rating_to;not null"`
-	Time        string `gorm:"not null"`
-	ChangePct    float64
-	CurrentPrice float64
+	Time        string `gorm:"column:time;not null"`
 }
 
 type StockPriceHistory struct {
 	gorm.Model
 	StockID     uint      `gorm:"index"`
 	Ticker      string    `gorm:"index"`
-	Price       float64
+	TargetFrom  float64
+	TargetTo    float64
 	Timestamp   time.Time `gorm:"index"`
 }
 
@@ -37,14 +36,25 @@ func RunMigrations(db *gorm.DB) error {
 		if err := db.Exec("DROP TABLE IF EXISTS stocks").Error; err != nil {
 			return err
 		}
+
 		log.Println("Tabla stocks eliminada")
+
+		if err := db.Exec("DROP TABLE IF EXISTS stock_price_histories").Error; err != nil {
+			return err
+		}
+
+		log.Println("Tabla stock_price_histories eliminada")
+
+		// Ejecutar migraciones
+		if err := db.AutoMigrate(&Stock{}, &StockPriceHistory{}); err != nil {
+			return err
+		}
+
+		log.Println("Migración completada")
+	} else {
+		log.Println("Migración no requerida")
 	}
 
-	// Ejecutar migraciones
-	if err := db.AutoMigrate(&Stock{}, &StockPriceHistory{}); err != nil {
-		return err
-	}
 
-	log.Println("Migración completada")
 	return nil
 }
