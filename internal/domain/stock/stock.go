1624290aaefa9481bf2f77bdeeec707d9fb77464
Odannys Enrique De La Cruz Calvo <el_odanis321@hotmail.com>
2025-03-17 13:26:05 -0500

    feat (internal): crear paquete domain para organizar entidades de alto nivel

---
+++ internal/domain/stock/stock.go
@@ -0,0 +1,25 @@
+package stock
+
+import (
+	"time"
+
+	"github.com/OdannysDeLaCruz/stock-tracker/internal/domain/price_history"
+	"gorm.io/gorm"
+)
+
+// Stock representa la entidad de un stock
+type Stock struct {
+	gorm.Model
+	Ticker      string `gorm:"column:ticker;uniqueIndex"`
+	TargetFrom  float64 `gorm:"column:target_from;not null"`
+	TargetTo    float64 `gorm:"column:target_to;not null"`
+	Company     string `gorm:"column:company;not null"`
+	Action      string `gorm:"column:action;not null"`
+	Brokerage   string `gorm:"column:brokerage;not null"`
+	RatingFrom  string `gorm:"column:rating_from;not null"`
+	RatingTo    string `gorm:"column:rating_to;not null"`
+	Time        time.Time `gorm:"column:time;not null;type:timestamp"`
+
+    // Relaciones
+    PriceHistory []price_history.StockPriceHistory `gorm:"foreignKey:StockID;constraint:OnDelete:CASCADE"`
+}
